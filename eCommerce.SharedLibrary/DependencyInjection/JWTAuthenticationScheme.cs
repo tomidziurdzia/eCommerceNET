a85fae550e03bff7413e973906cc658ebae4bef2
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2024-08-27 19:47:27 +0200

    Add JWT Authentication Scheme

---
+++ eCommerce.SharedLibrary/DependencyInjection/JWTAuthenticationScheme.cs
@@ -0,0 +1,36 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace eCommerce.SharedLibrary.DependencyInjection;
+
+public static class JWTAuthenticationScheme
+{
+    public static IServiceCollection AddJWTAuthenticationScheme(this IServiceCollection services, IConfiguration config)
+    {
+        // Add JWT Service
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer("Bearer", options =>
+            {
+                var key = Encoding.UTF8.GetBytes(config.GetSection("Authentication:Key").Value!);
+                string issuer = config.GetSection("Authentication:Issuer").Value!;
+                string audience = config.GetSection("Authentication:Audience").Value!;
+
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidateAudience = true,
+                    ValidateIssuer = true,
+                    ValidateLifetime = false,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = issuer,
+                    ValidAudience = audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(key)
+                };
+            });
+        return services;
+    }
+}
