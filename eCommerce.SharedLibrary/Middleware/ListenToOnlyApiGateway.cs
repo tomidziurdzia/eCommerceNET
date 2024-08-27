1e8f5c75d0cea6972e36e8f2264a9eedddff93b5
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2024-08-27 19:36:43 +0200

    Add Api Gateway

---
+++ eCommerce.SharedLibrary/Middleware/ListenToOnlyApiGateway.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Http;
+
+namespace eCommerce.SharedLibrary.Middleware;
+
+public class ListenToOnlyApiGateway(RequestDelegate next)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        // Extract specific header from the request
+        var signedHeader = context.Request.Headers["Api-Gateway"];
+        
+        // Null means the request is not coming from the Api Gateway // 503 service unavailable
+        if (signedHeader.FirstOrDefault() is null)
+        {
+            context.Response.StatusCode = StatusCodes.Status503ServiceUnavailable;
+            await context.Response.WriteAsync("Sorry, service is unavailable");
+            return;
+        }
+        else
+        {
+            await next(context);
+        }
+    }
+}
